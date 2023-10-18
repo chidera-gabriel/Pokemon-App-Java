9153f475f6deb09c334f55a960381f984786d55e
W0476462 <w0476462@nscc.ca>
2023-10-18 16:45:40 -0300

    Added the Pokemon image and the two classes

---
+++ app/src/main/java/com/example/pokemonapp/retrofit/RetrofitApi.java
@@ -0,0 +1,14 @@
+package com.example.pokemonapp.retrofit;
+
+public interface RetrofitApi {
+
+    // This is the hostname for the API
+
+    String BASE_URL = "https://pokeapi.co";
+
+    // API call to get the forecast from the Api
+
+//    @Get(pokeapi.co/api/v2/) {
+//
+//    }
+}
