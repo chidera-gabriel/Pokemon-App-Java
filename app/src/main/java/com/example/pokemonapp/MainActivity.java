af02e1d8369b95babc6a035999b76000e4ff31e7
W0476462 <w0476462@nscc.ca>
2023-10-20 21:04:32 -0300

    Added getters and made improvements on the GUI app.

--- app/src/main/java/com/example/pokemonapp/MainActivity.java
+++ app/src/main/java/com/example/pokemonapp/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.pokemonapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.pokemonapp.databinding.ActivityMainBinding;
+import com.example.pokemonapp.models.Pokemon;
+import com.example.pokemonapp.retrofit.RetrofitClient;
+
+import retrofit2.Call;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        // Creating and initializing the Api  client
+        Call<Pokemon> call = RetrofitClient.getInstance().getApi().pokemonImage();
+        Call<Pokemon> call1 = RetrofitClient.getInstance().getApi().getName();
+        Call<Pokemon> call2 = RetrofitClient.getInstance().getApi().getType();
+        Call<Pokemon> call3 = RetrofitClient.getInstance().getApi().getHeight();
+        Call<Pokemon> call4 = RetrofitClient.getInstance().getApi().getWeight();
+
+
+
+
+
+
+
+
     }
 }
--- app/src/main/java/com/example/pokemonapp/retrofit/RetrofitApi.java
+++ app/src/main/java/com/example/pokemonapp/retrofit/RetrofitApi.java
@@ -1,14 +1,28 @@
 package com.example.pokemonapp.retrofit;
 
+import com.example.pokemonapp.models.Pokemon;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 public interface RetrofitApi {
 
     // This is the hostname for the API
 
     String BASE_URL = "https://pokeapi.co";
 
-    // API call to get the forecast from the Api
+     //API call to get the forecast from the Api
+    @GET("api/v2/pokemon")
+    Call<Pokemon>pokemonImage();
 
-//    @Get(pokeapi.co/api/v2/) {
-//
-//    }
+    @GET("api/v2/pokemon")
+    Call<Pokemon>getName();
+
+    @GET("api/v2/pokemon")
+    Call<Pokemon>getType();
+
+    @GET("api/v2/pokemon")
+    Call<Pokemon>getHeight();
+    @GET("api/v2/pokemon")
+    Call<Pokemon>getWeight();
 }
